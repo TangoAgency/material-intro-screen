c76ac3f725a37e90a5dbab91214f8e262b5923fd
bezmian <filip@norvox.pl>
2016-09-12 16:21:30 +0200

    Initial commit

--- app/src/main/java/agency/tango/materialintro/IntroActivity.java
+++ app/src/main/java/agency/tango/materialintro/IntroActivity.java
@@ -11,7 +11,6 @@ import android.widget.Toast;
 
 public class IntroActivity extends MaterialIntroActivity
 {
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
