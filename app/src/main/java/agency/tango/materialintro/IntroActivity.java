4293aa9ee95751744f2eac5a3e7c5b4baf967a34
bezmian <filip@norvox.pl>
2016-09-13 16:56:16 +0200

    * Improving intro screen view

--- app/src/main/java/agency/tango/materialintro/IntroActivity.java
+++ app/src/main/java/agency/tango/materialintro/IntroActivity.java
@@ -9,66 +9,59 @@ import android.widget.Toast;
 import agency.tango.materialintroscreen.MaterialIntroActivity;
 import agency.tango.materialintroscreen.SlideFragmentBuilder;
 
-public class IntroActivity extends MaterialIntroActivity
-{
+public class IntroActivity extends MaterialIntroActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         enableLastSlideAlphaExitTransition(true);
 
         addSlide(new SlideFragmentBuilder()
-                .primaryColor(R.color.colorAccent)
-                .secondaryColor(R.color.colorPrimary)
-                .image(agency.tango.materialintroscreen.R.drawable.ic_next)
-                .title("title 1")
-                .description("Description 1")
-                .messageButtonText("lol")
-                .messageButtonClickListener(new View.OnClickListener()
-                {
+                .primaryColor(R.color.first_slide_primary)
+                .secondaryColor(R.color.first_slide_secondary)
+                .image(R.drawable.img_office)
+                .title("Organize your time with us")
+                .description("Would you try?")
+                .messageButtonText("Work with love")
+                .messageButtonClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View v)
-                    {
-                        Toast.makeText(getApplicationContext(), "I really love android", Toast.LENGTH_SHORT).show();
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "Love with work", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .build());
 
         addSlide(new SlideFragmentBuilder()
-                .primaryColor(R.color.colorPrimary)
-                .secondaryColor(R.color.colorAccent)
-                .title("title 2")
-                .description("Description 2")
+                .primaryColor(R.color.second_slide_primary)
+                .secondaryColor(R.color.second_slide_secondary)
+                .title("Want more?")
+                .description("Go on")
                 .build());
 
         addSlide(new CustomSlide());
 
         addSlide(new SlideFragmentBuilder()
-                .primaryColor(R.color.colorPrimary)
-                .secondaryColor(R.color.colorAccent)
+                .primaryColor(R.color.third_slide_primary)
+                .secondaryColor(R.color.third_slide_secondary)
                 .possiblePermissions(new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.READ_SMS})
                 .neededPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
-                .image(agency.tango.materialintroscreen.R.drawable.ic_next)
-                .messageButtonText("Android")
-                .messageButtonClickListener(new View.OnClickListener()
-                {
+                .image(R.drawable.img_equipment)
+                .messageButtonText("Tools")
+                .messageButtonClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View v)
-                    {
-                        Toast.makeText(getApplicationContext(), "Hate eating apples.", Toast.LENGTH_SHORT).show();
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "Try us", Toast.LENGTH_SHORT).show();
                     }
                 })
-                .title("title 3")
-                .description("Description 3")
+                .title("We provide best tools")
+                .description("ever")
                 .build());
 
 
         addSlide(new SlideFragmentBuilder()
                 .primaryColor(R.color.colorAccent)
                 .secondaryColor(R.color.colorPrimary)
-                .title("title 4")
-                .description("Description 4")
+                .title("That's it")
+                .description("Would you join us?")
                 .build());
     }
-
 }
